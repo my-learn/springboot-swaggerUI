05ee3cb9ad0b4fe82246026372b5af681cf1086d
maskleo <masque@aliyun.com>
2018-05-22 22:31:02 +0800

    init

---
+++ swaggerui/src/main/java/edu/maskleo/swaggerui/SwaggeruiApplication.java
@@ -0,0 +1,12 @@
+package edu.maskleo.swaggerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggeruiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggeruiApplication.class, args);
+	}
+}
